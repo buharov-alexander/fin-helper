e92472a2fb146d954c5d2a3a2f1b90a13ffc9d41
Alexander Bukharov <buharovsasha@yandex.ru>
2023-08-20 18:01:06 +0700

    Add accounts service

---
+++ fh-accounts/src/main/kotlin/com/bukharov/fh/fhaccounts/FhAccountsApplication.kt
@@ -0,0 +1,11 @@
+package com.bukharov.fh.fhaccounts
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FhAccountsApplication
+
+fun main(args: Array<String>) {
+	runApplication<FhAccountsApplication>(*args)
+}
---
+++ fh-accounts/src/test/kotlin/com/bukharov/fh/fhaccounts/FhAccountsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bukharov.fh.fhaccounts
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FhAccountsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
