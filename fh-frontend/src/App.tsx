19dfcaf7f52bb7d76aec3d311386aba5c326ca8d
Alexander Bukharov <buharovsasha@yandex.ru>
2023-09-01 17:44:21 +0700

    add AppBar

--- fh-frontend/src/App.tsx
+++ fh-frontend/src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
+
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 import SummaryPage from 'pages/Summary';
 
 function App() {
   return (
     <div className="App">
-      <SummaryPage />
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <Typography variant="h6" color="inherit" component="div">
+            Finance
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <div className='main'>
+        <SummaryPage />
+      </div>
     </div>
   );
 }
