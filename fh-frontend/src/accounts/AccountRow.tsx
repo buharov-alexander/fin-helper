66dda66d42912b3995208c52ce4706462788b195
Alexander Bukharov <buharovsasha@yandex.ru>
2023-09-21 23:19:25 +0700

    add input

--- fh-frontend/src/accounts/AccountRow.tsx
+++ fh-frontend/src/accounts/AccountRow.tsx
@@ -1,10 +1,11 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement, useState } from 'react';
 import { useAppDispatch } from 'app/hooks';
 import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+import TextField from '@mui/material/TextField';
 
 import { Account, deleteAccount } from 'accounts/accountsSlice'
 
@@ -15,12 +16,22 @@ interface AccountRowProps {
 
 const AccountRow: FC<AccountRowProps> = ({ account }): ReactElement => {
     const dispatch = useAppDispatch();
+    const [amount, setAmount] = useState(account.balance.amount.toString());
+
 
     return (
         <TableRow>
             <TableCell component="th" scope="row">{account.id}</TableCell>
             <TableCell align="right">{account.name}</TableCell>
-            <TableCell align="right">{account.balance.amount}</TableCell>
+            <TableCell align="right">
+                <TextField
+                    id="balance-amount"
+                    variant="standard"
+                    defaultValue={account.balance.amount}
+                    error={isNaN(Number(amount))}
+                    onChange={(event) => setAmount(event.target.value)}
+                />
+            </TableCell>
             <TableCell align="right">{account.balance.currency}</TableCell>
             <TableCell align="right">
                 <IconButton aria-label="edit">
