f73884ca0c0acca391db65f62d4bbf0eba02620c
Alexander Bukharov <buharovsasha@yandex.ru>
2023-10-15 00:47:26 +0700

    add Breadcrumbs

--- fh-frontend/src/pages/AccountPage.tsx
+++ fh-frontend/src/pages/AccountPage.tsx
@@ -3,6 +3,10 @@ import { useParams } from 'react-router-dom';
 
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+import { Link as RouterLink } from "react-router-dom";
 
 import AccountHistoryTable from 'accounts/historyTable/AccountHistoryTable';
 import { useAppDispatch, useAppSelector } from 'app/hooks';
@@ -19,6 +23,17 @@ export default function AccountPage() {
 
     return (
         <Box sx={{ flexGrow: 1 }}>
+            <Breadcrumbs aria-label="breadcrumb" className='b-breadcrumb'>
+                <Link
+                    underline="hover"
+                    color="inherit"
+                    to="/"
+                    component={RouterLink as any}
+                >
+                    Accounts
+                </Link>
+                <Typography color="text.primary">{id}</Typography>
+            </Breadcrumbs>
             <Grid container spacing={2}>
                 <Grid item xs={12}>
                     <AccountHistoryTable accountHistory={accountHistory} />
