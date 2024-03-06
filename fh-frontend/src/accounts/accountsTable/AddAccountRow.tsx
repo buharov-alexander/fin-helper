d642b95c26eccda7d1301848d1d1fd5679a33892
Alexander Bukharov <buharovsasha@yandex.ru>
2024-03-06 22:50:53 +0700

    add form for new account name

--- fh-frontend/src/accounts/accountsTable/AddAccountRow.tsx
+++ fh-frontend/src/accounts/accountsTable/AddAccountRow.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
+import TextField from '@mui/material/TextField';
 
 const AddAccountRow = () => {
+    const [_name, setName] = useState("");
+    const [_isEdit, setIsEdit] = useState(false);
+
+    const rowView = _isEdit ? (
+        <TextField
+            id="account-name"
+            variant="standard"
+            error={!_name}
+            onChange={(event) => setName(event.target.value)}
+            autoFocus
+        />
+    ) : "Add New Account";
+
+    const updateEditMode = (value: boolean) => {
+        if (value === _isEdit) {
+            return;
+        }
+        setIsEdit(value);
+    }
+
     return (
-        <TableRow className="b-add-account-row">
-            <TableCell colSpan={5}>Add New Account</TableCell>
+        <TableRow
+            className="b-add-account-row"
+            onClick={() => updateEditMode(true)}
+            onBlur={() => updateEditMode(false)}
+        >
+            <TableCell colSpan={5}>{rowView}</TableCell>
         </TableRow>
     );
 }
